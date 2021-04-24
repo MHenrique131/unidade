4f8ee56f17087d9a85e64bffbec7d13bf4e01e36
MHenrique131 <mateus11medeirosss@gmail.com>
2021-04-24 09:28:05 -0300

    primeiro commit

---
+++ exercicio023.py
@@ -0,0 +1,8 @@
+n = int(input('digite um número: '))
+u = n % 10
+d = n // 10 % 10
+c = n // 100 % 10
+m = n // 1000 % 10
+print(f'analisando o número {n}...')
+print(f'unidade: {u} \n dezena {d} \n centena: {c} \n milhar: {m}')
+#git init git status git add git commit -m "mensagem"   git push -u origin master
